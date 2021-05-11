a72f3f32b48e79a8a5a888a5bc06788745fef0e7
Anurag Tilwe <anurag.tilwe6@gmail.com>
2021-05-11 11:10:19 -0400

    Activity icons

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -8,11 +8,12 @@ import android.widget.*;
 public class MainActivity extends AppCompatActivity {
 
     ImageView iv_home, iv_meals, iv_diary, iv_activity, iv_info;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         iv_home = findViewById(R.id.id_image_home);
         iv_meals = findViewById(R.id.id_image_meals);
         iv_diary = findViewById(R.id.id_image_diary);

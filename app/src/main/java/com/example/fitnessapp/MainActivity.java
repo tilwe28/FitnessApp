a608c8437fd30b475a606f2f89f2479af6a80b9c
Anurag <anurag.tilwe6@gmail.com>
2021-05-11 11:05:29 -0400

    Icons for changing activities

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView iv_home, iv_meals, iv_diary, iv_activity, iv_info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_home = findViewById(R.id.id_image_home);
+        iv_meals = findViewById(R.id.id_image_meals);
+        iv_diary = findViewById(R.id.id_image_diary);
+        iv_activity = findViewById(R.id.id_image_activity);
+        iv_info = findViewById(R.id.id_image_personalInfo);
+        
     }
 }

96f48aa23a18668422b011efe45030197b7c49dd
Anurag <anurag.tilwe6@gmail.com>
2021-05-14 15:39:59 -0400

    more with user info

--- app/src/main/java/com/example/fitnessapp/Info.java
+++ app/src/main/java/com/example/fitnessapp/Info.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.*;
 
 public class Info extends AppCompatActivity {
 
     ImageView iv_homeIcon, iv_mealsIcon, iv_diaryIcon, iv_exerciseIcon, iv_infoIcon;
-    TextView tv_heading;
+    TextView tv_heading, tv_name, tv_height, tv_weight, tv_age, tv_weightGoal;
+    EditText et_name, et_height, et_weight, et_age;
+    RadioGroup rg_weightGoal;
+    RadioButton rb_lose, rb_gain, rb_maintain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,5 +64,22 @@ public class Info extends AppCompatActivity {
         });
 
         tv_heading = findViewById(R.id.id_info_textView_heading);
+        tv_name = findViewById(R.id.id_info_textView_name);
+        tv_height = findViewById(R.id.id_info_textView_height);
+        tv_weight = findViewById(R.id.id_info_textView_weight);
+        tv_age = findViewById(R.id.id_info_textView_age);
+        et_name = findViewById(R.id.id_info_editText_name);
+        et_height = findViewById(R.id.id_info_editText_height);
+        et_weight = findViewById(R.id.id_info_editText_weight);
+        et_age = findViewById(R.id.id_info_editText_age);
+        rg_weightGoal = findViewById(R.id.id_info_radioGroup_weightGoal);
+        rb_lose = findViewById(R.id.id_info_radioButton_lose);
+        rb_gain = findViewById(R.id.id_info_radioButton_gain);
+        rb_maintain = findViewById(R.id.id_info_radioButton_maintain);
+    }
+
+    public void clearText(View v) {
+        EditText et = findViewById(v.getId());
+        et.setText("");
     }
 }

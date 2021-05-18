53609f770edc5b81ab5e6da7118dcbcbe80f8440
Anurag <anurag.tilwe6@gmail.com>
2021-05-18 12:31:50 -0400

    Data class

---
+++ app/src/main/java/com/example/fitnessapp/Data.java
@@ -0,0 +1,40 @@
+package com.example.fitnessapp;
+
+public class Data {
+
+    private static String name="";
+    private static double height=0.0, weight=0.0, calories=0.0;
+    private static int age=0;
+
+    public static String getName() {
+        return name;
+    }
+    public static double getHeight() {
+        return height;
+    }
+    public static double getWeight() {
+        return weight;
+    }
+    public static double getCalories() {
+        return calories;
+    }
+    public static int getAge() {
+        return age;
+    }
+
+    public static void setName(String newName) {
+        name = newName;
+    }
+    public static void setHeight(double newHeight) {
+        height = newHeight;
+    }
+    public static void setWeight(double newWeight) {
+        weight = newWeight;
+    }
+    public static void setCalories(double newCalories) {
+        calories = newCalories;
+    }
+    public static void setAge(int newAge) {
+        age = newAge;
+    }
+}
